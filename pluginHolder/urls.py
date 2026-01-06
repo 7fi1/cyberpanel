86c937d49a89843c8b08b9c81cdd23821d9a492c
master3395 <master3395@users.noreply.github.com>
2026-01-06 19:28:40 +0100

    Revert "Refactor: replace url() with path() for Django routes in plugin Installer"
    This reverts commit 553b4ccf5463195795efb37d092ab8fb9c9b4677.

--- pluginHolder/urls.py
+++ pluginHolder/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("installed", views.installed, name="installed"),
-    path("api/install/<str:plugin_name>/", views.install_plugin, name="install_plugin"),
-    path("api/uninstall/<str:plugin_name>/", views.uninstall_plugin, name="uninstall_plugin"),
-    path("api/enable/<str:plugin_name>/", views.enable_plugin, name="enable_plugin"),
-    path("api/disable/<str:plugin_name>/", views.disable_plugin, name="disable_plugin"),
+    path('installed', views.installed, name='installed'),
 ]
